132730aa7a4b4103d82addde0e155769724fd3ac
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-17 19:41:07 +0300

    Create and generate model Comment user:references:index post:references:index text:text

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :author, foreign_key: :author_id, class_name: 'User'
-    has_many :comments
-    has_many :likes
-  
-    after_create :update_posts_counter
-  
-    def recent_comments
-      comments.where(post_id: id).limit(5).order(created_at: :desc)
-    end
-  
-    private
-  
-    def update_posts_counter
-      author.increment!(:posts_counter)
-    end
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
+  has_many :comments
+  has_many :likes
+
+  after_create :update_posts_counter
+
+  def recent_comments
+    comments.where(post_id: id).limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
