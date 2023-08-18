d0113576103f48b580f1f5c68223124a6665c1a9
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-18 15:33:49 +0300

    # ADD validation For the Post model: Title must not be blank & not exceed 250 characters

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,15 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   after_save :update_author_posts_counter
 
+  # ADD validation For the Post model: Title must not be blank & not exceed 250 characters
+  validates :title, presence: true, length: { maximum: 250 }
+
+  # CommentsCounter must be an integer greater than or equal to zero
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  # LikesCounter must be an integer greater than or equal to zero
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def last_5_comments
     comments.order(created_at: :desc).limit(5)
   end
