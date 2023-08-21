6ad390413f4a27e9db0c9dd995a62b0ac372e8b4
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-22 02:46:15 +0300

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/_post'
-  get 'posts/_full'
-  get 'posts/_comment'
-  get 'users/index'
-  get 'users/show'
-  get 'users/_user'
-  get 'users/_bio'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  root 'users#index'
 end
