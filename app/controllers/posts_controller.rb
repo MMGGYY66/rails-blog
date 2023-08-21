1f999f769980b9786665fbe96e549717164e7ffe
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-22 02:35:25 +0300

    Create views for posts : index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,5 @@
 class PostsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
-  def _post
-  end
-
-  def _full
-  end
-
-  def _comment
-  end
+  def show; end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,53 +1,52 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/posts/index"
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/posts/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/posts/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/posts/show'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/posts/new"
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/posts/new'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/posts/edit"
+  describe 'GET /edit' do
+    it 'returns http success' do
+      get '/posts/edit'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /_post" do
-    it "returns http success" do
-      get "/posts/_post"
+  describe 'GET /_post' do
+    it 'returns http success' do
+      get '/posts/_post'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /_full" do
-    it "returns http success" do
-      get "/posts/_full"
+  describe 'GET /_full' do
+    it 'returns http success' do
+      get '/posts/_full'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /_comment" do
-    it "returns http success" do
-      get "/posts/_comment"
+  describe 'GET /_comment' do
+    it 'returns http success' do
+      get '/posts/_comment'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
