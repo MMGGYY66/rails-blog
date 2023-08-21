43ece02166f87beed445dd3b4f89eb92f04aea96
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-22 00:19:15 +0300

    Update Database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: :author_id, dependent: :destroy
-  has_many :comments, foreign_key: :author_id, dependent: :destroy
-  has_many :likes, foreign_key: :author_id, dependent: :destroy
+  has_many :posts, foreign_key: :author_id # dependent: :destroy
+  has_many :comments, foreign_key: :author_id # dependent: :destroy
+  has_many :likes, foreign_key: :author_id # dependent: :destroy
 
   # ADD validation to User model : Name must not be blank
   validates :name, presence: true
