91d9525f79e3b81e5f2bd2bea7f72119904b3891
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-23 03:03:36 +0300

    Create rspec tests for posts controller2

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'UsersController', type: :request do
+RSpec.describe 'PostsController', type: :request do
   let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
   let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
 
