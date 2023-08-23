7b73c330dd0d819888f390bebdcd8d89764772ee
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-23 21:59:16 +0300

    Update posts_spec.rb test and fix linters issues

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe 'PostsController', type: :request do
-  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+RSpec.describe 'Posts', type: :request do
+  let!(:user) { User.create(name: 'John', posts_counter: 0) }
 
-  describe 'GET /users/:id/posts' do
-    it 'renders the index template and includes placeholder text' do
+  describe 'GET /index' do
+    it 'response status is success' do
       get user_posts_path(user)
-      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+    end
+    it 'index template was rendered' do
+      get user_posts_path(user)
       expect(response).to render_template(:index)
-      expect(response.body).to include("<button class='btn btn-primary'>Pagination</button>")
+    end
+    it "response body includes the text 'List of posts'" do
+      get user_posts_path(user)
+      expect(response.body).to include('List of posts')
     end
   end
 
-  describe 'GET /users/:id/posts/:post_id' do
-    it 'renders the show template and includes placeholder text' do
+  describe 'GET /show' do
+    let!(:post) { Post.create(author: user, title: 'Post title', comments_counter: 0, likes_counter: 0) }
+
+    it 'response status is success' do
       get user_post_path(user, post)
-      expect(response).to have_http_status(200)
+      expect(response.status).to eq(200)
+    end
+    it 'show template was rendered' do
+      get user_post_path(user, post)
       expect(response).to render_template(:show)
-      expect(response.body).to include('<h5 class="card-title">Post X by username</h5>')
+    end
+    it "response body includes the text 'Post by'" do
+      get user_post_path(user, post)
+      expect(response.body).to include('Post by')
     end
   end
 end
