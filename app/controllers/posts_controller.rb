262fcbcfdd89681a8e9c434cc8d7e9d0edcb34b4
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-24 22:45:39 +0300

    Update PostsController with new and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,26 @@ class PostsController < ApplicationController
     @posts = @user.last_3_posts.includes(:comments)
   end
 
+  def new
+    @post = Post.new
+  end
+
   def show
     @post = Post.includes(:comments).find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post.author
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:author_id, :title, :text)
   end
 end
