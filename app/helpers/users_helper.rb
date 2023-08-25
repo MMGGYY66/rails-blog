4fcc7783725e30f721bda1cbaae0faa5180b9158
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-08-25 04:16:28 +0300

    Create add_post_link method inside UserHelper module and fix linters issues

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def add_post_link(user)
+    return unless current_user.present?
+    return unless current_user.id == user.id
+
+    link_to 'Add Post', new_post_path, class: 'btn btn-primary'
+    button_tag('Add Post') do
+      link_to 'Add Post', new_post_path, class: 'btn btn-primary'
+    end
+    render partial: 'posts/new', locals: { current_user: @current_user }
+    form_for @post, url: @post.to_s, method: :get do |f|
+      f.submit 'Add Post', class: 'btn btn-primary'
+    end
+  end
 end
